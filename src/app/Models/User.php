2dc914371f6f5c203deed580b1614984f12fa771
Alan Diniz <alan@splitpaybrasil.com.br>
2024-06-03 16:29:21 -0300

    update model user

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,15 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
-    // Outros métodos e propriedades
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'type',
+        'atived',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array<string, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * The courses that belong to the user.
@@ -19,5 +40,25 @@ class User extends Authenticatable
     public function courses()
     {
         return $this->belongsToMany(Course::class)->withPivot('data_inscricao', 'valor_pago', 'status')->withTimestamps();
+    }
+
+    /**
+     * Get the identifier that will be stored in the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
